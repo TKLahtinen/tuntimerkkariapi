bac112c8549f3c8adb7117ad635a12b9cdc1db2b
TKLahtinen <n2lati00@students.oamk.fi>
2024-08-23 14:58:23 +0300

    Project model created

---
+++ app/models/project.rb
@@ -0,0 +1,12 @@
+class Project < ApplicationRecord
+    enum status: { active: 0, archived: 1 }
+
+    validates :name, presence: true
+    validates :status, presence: true
+
+    after_initialize :set_default_status, if: :new_record?
+
+        def set_default_status
+            self.status ||= :active
+        end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_23_104740) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_23_115146) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name", null: false
+    t.date "start_date", null: false
+    t.integer "status", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
