ff80fdf3abc3d7dfa5019b085fcebf246f0970c7
TKLahtinen <n2lati00@students.oamk.fi>
2024-08-23 14:01:40 +0300

    implemented roles for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
+
+  enum role: { user: 0, admin: 1 }
+  after_initialize :set_default_role, if: :new_record?
+  
+    def set_default_role
+      self.role ||= :user
+    end
 end
